4781ffcad3dc9571a295a1d628b8e019563b565a
PyPhu <tawanpurin@gmail.com>
2026-01-28 14:52:18 +0700

    Update HTML CSS JS layout

--- frontend/src/components/sign_in.js
+++ frontend/src/components/sign_in.js
@@ -2,3 +2,16 @@ function showForm(formID){
     document.querySelectorAll(".form-box").forEach(form => {form.classList.remove("active");});
     document.getElementById(formID).classList.add("active");
 }
+
+function previewAvatar(event) {
+    const file = event.target.files[0];
+    const avatar = document.getElementById("avatarPreview");
+
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = () => {
+            avatar.src = reader.result;
+        };
+        reader.readAsDataURL(file);
+    }
+}
